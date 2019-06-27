db20895cc6bb6ce427f9009a50fe54f1cb6090f5
Federico Cuenca <fcuenca@domsf.com>
2019-06-27 18:24:43 -0300

    Version casi final

--- SimulacionPool.py
+++ SimulacionPool.py
@@ -1,5 +1,7 @@
 from graphics import *
 from math import *
+from multiprocessing  import Process
+
 import sys
 import time
 
@@ -8,7 +10,7 @@ Lroz = 10.0
 
 T = 150
 
-M = 5
+M = 2
 X0 = 100
 Y0 = 250
 V0 = 20
@@ -25,79 +27,83 @@ def choque(ball1, ball2):
         return False
 
 
-def Fuerza(v, b, angulo):
+def Fuerza(v, b):
 	return - b*v 
 
-def moverBlanca(blanca, click, negra):
+def mover(blanca, click, roja):
 	t0 = time.time()
 	t = 0
 	DeltaT = 1/60
 	n = 0
 	dist = ((blanca.getCenter().getX() - click.getX())**2 + (blanca.getCenter().getY() - click.getY())**2)**0.5
-	print("Dist: " + str(dist))
 	V = dist / M
-	print("V: " + str(V))
-	angulo = degrees(atan2(blanca.getCenter().getY() - click.getY(), blanca.getCenter().getX() - click.getX()))
-	print("angulo: " + str(angulo))
-	while (V > 0):
+	angBlanca = degrees(atan2(blanca.getCenter().getY() - click.getY(), blanca.getCenter().getX() - click.getX()))
+	VxBlanca = V * cos(radians(angBlanca))
+	VyBlanca = V * sin(radians(angBlanca))
+	VxRoja = 0
+	VyRoja = 0
+	while ( (abs(VxBlanca) >= 0.5 and abs(VyBlanca) >= 0.5) or (abs(VxRoja) >= 0.5 and abs(VyRoja) >= 0.5) ):
 		t = time.time() - t0
 		if(t >= n * DeltaT):
-			if(V > 0):
-				VTemp = V + DeltaT * Fuerza(V, 0.1, angulo) /M
-				Dx = VTemp * DeltaT * cos(radians(angulo))
-				Dy = VTemp * DeltaT * sin(radians(angulo))
-				V = VTemp
-				if(blanca.getCenter().getX() < 1000 - RADIO and blanca.getCenter().getX() > 0 + RADIO  and blanca.getCenter().getY() < 500 - RADIO and blanca.getCenter().getY() > 0 + RADIO):
-					blanca.move(Dx,Dy)
-				else:
-					angulo = degrees(radians(angulo) + radians(90))
-				if(choque(blanca, negra)):
-					moverNegra(negra, V, angulo, blanca)
-					V = 0
+			if(abs(VxBlanca) >= 0.5 and abs(VyBlanca) >= 0.5):
+				VxBlancaTemp = VxBlanca + DeltaT * (Fuerza(VxBlanca, 0.1)/M)
+				VyBlancaTemp = VyBlanca + DeltaT * (Fuerza(VyBlanca, 0.1)/M)
+				DxBlanca = VxBlancaTemp * DeltaT
+				DyBlanca = VyBlancaTemp * DeltaT
+				VxBlanca = VxBlancaTemp
+				VyBlanca = VyBlancaTemp
+				blanca.move(DxBlanca,DyBlanca)
+				if(abs(blanca.getCenter().getX() - 1000) <= RADIO or blanca.getCenter().getX() <= RADIO):
+					VxBlanca = - VxBlanca
+				if(abs(blanca.getCenter().getY() - 500) <= RADIO or blanca.getCenter().getY() <= RADIO):
+					VyBlanca = - VyBlanca
+			if(choque(blanca, roja)):
+				angChoque = degrees(atan2(roja.getCenter().getY() - blanca.getCenter().getY(), roja.getCenter().getX() - blanca.getCenter().getX()))
+				print("angChoque: " + str(angChoque))
+				VBlanca = (((VxBlanca)**2 + (VyBlanca)**2)**0.5)*sin(angChoque)
+				print("VBlanca: " + str(VBlanca))
+				VRoja = (((VxBlanca)**2 + (VyBlanca)**2)**0.5)*cos(angChoque)
+				print("VRoja: " + str(VRoja))
+				VxRoja = VRoja * cos(radians(angChoque))
+				print("VxRoja: " + str(VxRoja))
+				VyRoja = VRoja * sin(radians(angChoque))
+				print("VyRoja: " + str(VyRoja))
+				VxBlanca = VBlanca * sin(radians(90 - angChoque))
+				print("VxBlanca: " + str(VxBlanca))
+				VyBlanca = VBlanca * cos(radians(90 - angChoque))
+				print("VyBlanca: " + str(VyBlanca))
+			if(abs(VxRoja) >= 0.5 and abs(VyRoja) >= 0.5):
+				VxRojaTemp = VxRoja + DeltaT * (Fuerza(VxRoja, 0.1)/M)
+				VyRojaTemp = VyRoja + DeltaT * (Fuerza(VyRoja, 0.1)/M)
+				DxRoja = VxRojaTemp * DeltaT
+				DyRoja = VyRojaTemp * DeltaT
+				VxRoja = VxRojaTemp
+				VyRoja = VyRojaTemp
+				roja.move(DxRoja,DyRoja)
+				if(abs(roja.getCenter().getX() - 1000) <= RADIO or roja.getCenter().getX() <= RADIO):
+					VxRoja = - VxRoja
+				if(abs(roja.getCenter().getY() - 500) <= RADIO or roja.getCenter().getY() <= RADIO):
+					VyRoja = - VyRoja
 			n = n + 1
-				
-def moverNegra(negra, V, angulo, blanca):
-	t0 = time.time()
-	t = 0
-	DeltaT = 1/60
-	n = 0
-	angulo = degrees(atan2(negra.getCenter().getY() - blanca.getCenter().getY(), negra.getCenter().getX() - blanca.getCenter().getX()))
-	print("angulo: " + str(angulo))
-	while (V > 0):
-		t = time.time() - t0
-		if(t >= n * DeltaT):
-			if(V > 0):
-				VTemp = V + DeltaT * Fuerza(V, 0.1, angulo) /M
-				Dx = VTemp * DeltaT * cos(radians(angulo))
-				Dy = VTemp * DeltaT * sin(radians(angulo))
-				V = VTemp
-				if(negra.getCenter().getX() < 1000 - RADIO and negra.getCenter().getX() > 0 + RADIO  and negra.getCenter().getY() < 500 - RADIO and negra.getCenter().getY() > 0 + RADIO):
-					negra.move(Dx,Dy)
-				else:
-					angulo = degrees(radians(angulo) + radians(90))
-					print("Cambio angulo: " + str(angulo))
-					V = 0
-			n = n + 1
-
 
 def main():
 	WIN_SIZE_X = 1000
 	WIN_SIZE_Y = 500
 	win = GraphWin("Simulación 2", WIN_SIZE_X, WIN_SIZE_Y)
 	win.setBackground("green")
+	angChoque
 	blanca = Circle(Point(X0 + 200,Y0),RADIO)
 	blanca.setFill("white")
 	blanca.setOutline("white")
 	blanca.draw(win)
 
-	negra = Circle(Point(X0 + 800, Y0), RADIO)
-	negra.setFill("black")
-	negra.draw(win)
+	roja = Circle(Point(X0 + 800, Y0), RADIO)
+	roja.setFill("red")
+	roja.setOutline("red")
+	roja.draw(win)
 
 	while (not win.closed):
 		click = win.getMouse()
-		print("x: " + str(click.getX()))
-		print("y: " + str(click.getY()))
-		moverBlanca(blanca, click, negra)
+		mover(blanca, click, roja)
 
 main()
